9ee055148d83c08d7c549d91e1657b3379493874
Pavel Kuznetsov <pavel@hypertrack.io>
2024-01-03 13:02:28 +0200

    Use WebViewAssetLoader

--- android/demo/src/main/java/com/hypertrack/sdk/webview/android/demo/MainActivity.kt
+++ android/demo/src/main/java/com/hypertrack/sdk/webview/android/demo/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.hypertrack.sdk.webview.android.demo
 
 import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
 import android.os.PersistableBundle
-import android.util.Log
-import android.webkit.ConsoleMessage
-import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.webkit.WebViewAssetLoader
+import androidx.webkit.WebViewClientCompat
 import com.hypertrack.sdk.webview.android.HyperTrackWebViewJsApi
 
 class MainActivity : AppCompatActivity() {
@@ -19,12 +21,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        webView = findViewById(R.id.webView)
 
+        val assetLoader: WebViewAssetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
+            .build()
+
+        webView = findViewById(R.id.webView)
         webView?.apply {
             settings.javaScriptEnabled = true
             settings.domStorageEnabled = true
             settings.javaScriptCanOpenWindowsAutomatically = true
+            settings.allowFileAccessFromFileURLs = false
+            settings.allowUniversalAccessFromFileURLs = false
+            settings.allowFileAccess = false
+            settings.allowContentAccess = false
+
+            webViewClient = object : WebViewClientCompat() {
+                override fun shouldInterceptRequest(
+                    view: WebView?,
+                    request: WebResourceRequest
+                ): WebResourceResponse? {
+                    return assetLoader.shouldInterceptRequest(request.url)
+                }
+
+                @Suppress("OVERRIDE_DEPRECATION")
+                override fun shouldInterceptRequest(
+                    view: WebView?,
+                    url: String?
+                ): WebResourceResponse? {
+                    return assetLoader.shouldInterceptRequest(Uri.parse(url))
+                }
+            }
+
             webChromeClient = object : WebChromeClient() {
 
 //                override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
@@ -32,6 +60,7 @@ class MainActivity : AppCompatActivity() {
 //                    return true
 //                }
             }
+
             addJavascriptInterface(TestInterface, "TestInterface")
             addJavascriptInterface(
                 HyperTrackWebViewJsApi(this@MainActivity, webView), HyperTrackWebViewJsApi.API_NAME
@@ -39,7 +68,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         if (savedInstanceState == null) {
-            webView?.loadUrl("file:///android_asset/index.html")
+            // Assets are hosted under http(s)://appassets.androidplatform.net/assets/... .
+            // If the application's assets are in the "main/assets" folder this will read the file
+            // from "main/assets/www/index.html" and load it as if it were hosted on:
+            // https://appassets.androidplatform.net/assets/www/index.html
+            webView?.loadUrl("https://appassets.androidplatform.net/assets/index.html")
         }
     }
 
